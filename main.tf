83a9407e9ea4b2365150132b9224c4f29d84663d
Kumaran <kumaranbecse@gmail.com>
2022-07-27 12:51:18 +0530

    Local Application Deployed in AWS Elastic BeanStalk.

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
 provider "aws" {
   access_key = "<<>>"
   secret_key = "<<>>"
-  region = "ap-south-1"
+  region     = "ap-south-1"
 }
 
-data "aws_ami" "ubuntu" {
+/*ata "aws_ami" "ubuntu" {
   most_recent = true
 
   filter {
@@ -38,4 +38,56 @@ resource "aws_instance" "web" {
   tags = {
     Name = "HelloWorld"
   }
+}*/
+
+resource "aws_s3_bucket" "hrkumaran_s3_bucket_myapp" {
+  bucket = "hrkumaran-myapp-prod"
+  #acl = "private"
 }
+
+resource "aws_s3_bucket_object" "s3_bucket_object_myapp" {
+  bucket = aws_s3_bucket.hrkumaran_s3_bucket_myapp.id
+  key = "beanstalk/myapp"
+  source = "target/springboot-k8s-demo.jar"
+}
+
+resource "aws_elastic_beanstalk_application" "beanstalk_myapp" {
+  name        = "myapp"
+  description = "The description of my application"
+}
+
+resource "aws_elastic_beanstalk_application_version" "beanstalk_myapp_version" {
+  application = aws_elastic_beanstalk_application.beanstalk_myapp.name
+  bucket      = aws_s3_bucket.hrkumaran_s3_bucket_myapp.id
+  key         = aws_s3_bucket_object.s3_bucket_object_myapp.id
+  name        = "myapp-1.0.0"
+}
+
+resource "aws_elastic_beanstalk_environment" "beanstalk_myapp_env" {
+  name                = "hrkumaran-myapp-prod"
+  application         = aws_elastic_beanstalk_application.beanstalk_myapp.name
+  solution_stack_name = "64bit Amazon Linux 2 v3.2.16 running Corretto 11"
+  version_label       = aws_elastic_beanstalk_application_version.beanstalk_myapp_version.name
+
+  setting {
+    name      = "SERVER_PORT"
+    namespace = "aws:elasticbeanstalk:application:environment"
+    value     = "5000"
+  }
+
+  setting {
+    namespace = "aws:ec2:instances"
+    name      = "InstanceTypes"
+    value     = "t2.micro"
+  }
+
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "IamInstanceProfile"
+    value     = "aws-elasticbeanstalk-ec2-role"
+  }
+}
+
+
+
+
